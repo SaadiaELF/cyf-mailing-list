3afd337df7029f531d00ebd0a116881e27a51c9b
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-02 22:38:23 +0000

    changing ata back

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3005;
-const lists = new Map();
+const data = [
+  {
+    name: "staff",
+    members: ["talea@techtonica.org", "michelle@techtonica.org"],
+  },
+  {
+    name: "students",
+    members: ["chris@techtonica.org", "hamid@techtonica.org"],
+  },
+];
 
 app.use(express.json());
 app.use(cors());
 
 // Get all existing lists
-app.get("/lists", (req, res) => {
-  const listsArray = Array.from(lists.keys());
-  res.json(listsArray);
+app.get("/lists", function (request, response) {
+  let lists = data.map((list) => list.name);
+  response.json(lists);
 });
 
 app.listen(PORT, function () {
